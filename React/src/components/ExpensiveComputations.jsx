882a5955454191a8f7e5aad860275242b81bdd05
CodeCommandBD <shantokumar22@gmail.com>
2025-05-23 23:14:25 +0600

    update

--- React/src/components/ExpensiveComputations.jsx
+++ React/src/components/ExpensiveComputations.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const ExpensiveComputations = () => {
+
+    let ApiData = useRef()
+    let pTag = useRef()
+
+    const fetchData =async () =>{
+       const respons = await fetch('https://jsonplaceholder.typicode.com/users')
+        ApiData.current = await respons.json()
+
+    }
+    const showData = () =>{
+        pTag.current.innerText = JSON.stringify(ApiData.current)
+    } 
+
   return (
     <div>
-        
+        <p ref={pTag}></p>
+        <button onClick={fetchData}>Call Data</button>
+        <button onClick={showData}>Show Data Data</button>
     </div>
   )
 }
