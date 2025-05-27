38ea97c97812c2bf6687e7b2f0f0d2e7233a571f
CodeCommandBD <shantokumar22@gmail.com>
2025-05-27 22:55:47 +0600

    update

--- React/src/pages/Product.jsx
+++ React/src/pages/Product.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactDom from '../components/ReactDom'
 import { useNavigate } from 'react-router-dom'
-// import { useParams } from 'react-router-dom'
+
 
 const Product = () => {
 
   const navigate = useNavigate()
   const handel = () => {
     navigate('/home')
+    
   }
+  const [products, setProducts] = useState([])
+
+  
+  
+
+  useEffect(()=>{
+    ( async () => {
+      const response = await fetch('https://dummyjson.com/products')
+      const json = await response.json()
+      setProducts(json.products)
+      console.log(json.products);
+      
+    })()
+
+
+  },[])
+
+  
 
   return (
 
     
     <div>
-        
-        Product
+       {products && products.length > 0 &&
+
+        products.map((product, i)=>{
+          const {id, title, category, price} = product
+          return <div key={id}>
+            <h3>Name: {title}</h3>
+            <h4>Category: {category}</h4>
+            <h2>Price: {price}</h2>
+          </div>
+        })
+       }
        <button onClick={handel}>go to home</button>
     </div>
   )
